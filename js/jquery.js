d33efad06d764bb93b91e75a31042a93e9ffd980
Eray Öztürk <54137035+erayoeztuerkk@users.noreply.github.com>
2021-10-21 14:00:23 +0200

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,6 @@
+    $(document).ready(function () {
+    $("body").css('margin-left', '20px');
+    $("mainheader").css("margin-top", "20px");
+    $("maintext").css("color", "#0769AA");
+        alert("jahdjkh");
+    });
